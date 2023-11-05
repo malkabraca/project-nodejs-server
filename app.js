3817f0ac207bab287e0de1fe8e9397c208d2e0e0
malkabraca <m0502800840@gmail.com>
2023-11-05 15:13:02 +0200

    app3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 console.log("file", config.get("file"));
 
-// app.use(cors());
+app.use(cors());
 // app.use(cors({
 //   origin: 'https://clint-dgar.onrender.com'
 // }));

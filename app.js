9a09a67e045a353a9e33cbc7fda2dc27d2bb578e
malkabraca <m0502800840@gmail.com>
2023-11-05 14:56:43 +0200

    cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 console.log("file", config.get("file"));
 
-// app.use(cors());
+app.use(cors());
 app.use(logger)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

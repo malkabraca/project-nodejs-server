bfd30063bcd2e28cae7b47900e5a2d1d8957aa4e
malkabraca <m0502800840@gmail.com>
2023-11-06 10:03:47 +0200

    end

--- model/mongodb/connectToDB.js
+++ model/mongodb/connectToDB.js
@@ -2,14 +2,14 @@ const config = require("config");
 const mongoose = require("mongoose");
 const {NODE_ENV, DATABASE_URL_MONGO } = process.env;
 
-// console.log("con str", config.get("dbConfig.url"));
+console.log("con str", config.get("dbConfig.url"));
 
-// const connectToDB = () => {
-//   return mongoose.connect(config.get("dbConfig.url"));
-// };
 const connectToDB = () => {
-  return mongoose.connect(
-  NODE_ENV === 'development' ? DATABASE_URL_MONGO : 'mongodb://127.0.0.1:27017/final_project',
-);
-}
+  return mongoose.connect(config.get("dbConfig.url"));
+};
+// const connectToDB = () => {
+//   return mongoose.connect(
+//   NODE_ENV === 'production' ? DATABASE_URL_MONGO : 'mongodb://127.0.0.1:27017/final_project',
+// );
+// }
 module.exports = connectToDB;
